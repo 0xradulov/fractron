8598b1460e613567834d89e82e40d1f90b5d968b
0xradulov <0xradulov@proton.me>
2022-10-04 19:07:39 +0200

    can split and join vaults

---
+++ contracts/src/Fractron.sol
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {ERC721} from "solmate/tokens/ERC721.sol";
+
+contract NFTShare is ERC20 {
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 mintSupply,
+        address mintTo
+    ) payable ERC20(name, symbol, 18) {
+        _mint(mintTo, mintSupply);
+    }
+
+    /// @dev The allowance check happens when substracting the amount from the allowed amount. This operation will underflow (and revert) if the caller doesn't have enough allowance.
+    function burnFrom(address from, uint256 amount) public payable {
+        uint256 allowed = allowance[from][msg.sender]; // Saves gas for limited approvals.
+
+        if (allowed != type(uint256).max)
+            allowance[from][msg.sender] = allowed - amount;
+
+        _burn(from, amount);
+    }
+}
+
+contract Fractron {
+    /// @notice Thrown when trying to rejoin a token from a vault that doesn't exist
+    error VaultNotFound();
+    /// @notice Thrown when trying to create vault that can't exist
+    error InvalidVault();
+
+    /// @dev Parameters for vaults
+    /// @param nftContracts The ERC721 contracts for the fractionalized tokens
+    /// @param tokenIds The IDs of the fractionalized tokens
+    /// @param tokenSupply The amount of issued ERC20 tokens for this vault
+    /// @param tokenContract The ERC20 contract for the issued tokens
+    struct Vault {
+        ERC721[] nftContracts;
+        uint256[] tokenIds;
+        uint256 tokenSupply;
+        NFTShare tokenContract;
+    }
+
+    event VaultCreated(Vault vault);
+    event VaultDestroyed(Vault vault);
+
+    uint256 internal currentVaultId = 1;
+    mapping(uint256 => Vault) public vaultById;
+
+    function split(
+        ERC721[] calldata nftContracts,
+        uint256[] calldata tokenIds,
+        uint256 supply,
+        string memory name,
+        string memory symbol
+    ) public payable returns (uint256) {
+        if (nftContracts.length != tokenIds.length) revert InvalidVault();
+        NFTShare tokenContract = new NFTShare(name, symbol, supply, msg.sender);
+
+        Vault memory vault = Vault({
+            nftContracts: nftContracts,
+            tokenIds: tokenIds,
+            tokenSupply: supply,
+            tokenContract: tokenContract
+        });
+
+        emit VaultCreated(vault);
+
+        vaultById[currentVaultId] = vault;
+
+        for (uint256 i = 0; i < nftContracts.length; i++) {
+            nftContracts[i].transferFrom(
+                msg.sender,
+                address(this),
+                tokenIds[i]
+            );
+        }
+
+        return currentVaultId++;
+    }
+
+    function join(uint256 vaultId) public payable {
+        Vault memory vault = vaultById[vaultId];
+
+        if (address(vault.tokenContract) == address(0)) revert VaultNotFound();
+
+        delete vaultById[vaultId];
+
+        vault.tokenContract.burnFrom(msg.sender, vault.tokenSupply);
+        for (uint256 i = 0; i < vault.nftContracts.length; i++) {
+            vault.nftContracts[i].transferFrom(
+                address(this),
+                msg.sender,
+                vault.tokenIds[i]
+            );
+        }
+    }
+
+    function getVault(uint256 vaultId) public view returns (Vault memory) {
+        return vaultById[vaultId];
+    }
+
+    /// @dev This function ensures this contract can receive ERC721 tokens
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes memory
+    ) public payable returns (bytes4) {
+        return this.onERC721Received.selector;
+    }
+}
---
+++ contracts/src/test/Fractron.t.sol
@@ -0,0 +1,137 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+import {Vm} from "forge-std/Vm.sol";
+import {DSTest} from "ds-test/test.sol";
+import {stdError} from "forge-std/test.sol";
+import {ERC721} from "solmate/tokens/ERC721.sol";
+import {Fractron, NFTShare} from "../Fractron.sol";
+
+contract User {}
+
+contract TestNFT is ERC721("Test NFT", "TEST") {
+    uint256 public tokenId = 1;
+
+    function tokenURI(uint256) public pure override returns (string memory) {
+        return "test";
+    }
+
+    function mint() public returns (uint256) {
+        _mint(msg.sender, tokenId);
+
+        return tokenId++;
+    }
+}
+
+contract FractronTest is DSTest {
+    uint256 collection1_id1;
+    uint256 collection1_id2;
+    uint256 collection2_id1;
+    uint256 collection2_id2;
+    User internal user;
+    TestNFT internal collection1;
+    TestNFT internal collection2;
+    Vm internal hevm = Vm(HEVM_ADDRESS);
+    Fractron internal fractron;
+
+    ERC721[] internal nftContracts;
+    uint256[] internal nftIds;
+
+    event VaultCreated(Fractron.Vault vault);
+    event VaultDestroyed(Fractron.Vault vault);
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+
+    function setUp() public {
+        user = new User();
+        collection1 = new TestNFT();
+        collection2 = new TestNFT();
+        fractron = new Fractron();
+
+        collection1.setApprovalForAll(address(fractron), true);
+        collection2.setApprovalForAll(address(fractron), true);
+
+        hevm.prank(address(user));
+        collection1.setApprovalForAll(address(fractron), true);
+        hevm.prank(address(user));
+        collection2.setApprovalForAll(address(fractron), true);
+
+        collection1_id1 = collection1.mint();
+        collection1_id2 = collection1.mint();
+        collection2_id1 = collection2.mint();
+        collection2_id2 = collection2.mint();
+
+        nftContracts.push(collection1);
+        nftContracts.push(collection1);
+        nftContracts.push(collection2);
+        nftContracts.push(collection2);
+
+        nftIds.push(collection1_id1);
+        nftIds.push(collection1_id2);
+        nftIds.push(collection2_id1);
+        nftIds.push(collection2_id2);
+    }
+
+    function testCanSplitToken() public {
+        assertEq(collection1.ownerOf(collection1_id1), address(this));
+
+        hevm.expectEmit(true, true, false, true);
+        emit Transfer(address(0), address(this), 100 ether);
+
+        uint256 vaultId = fractron.split(
+            nftContracts,
+            nftIds,
+            100 ether,
+            "Fractionalised NFT",
+            "FRAC"
+        );
+
+        Fractron.Vault memory vault = fractron.getVault(vaultId);
+
+        for (uint256 i = 0; i < vault.nftContracts.length; i++) {
+            assertEq(
+                vault.nftContracts[i].ownerOf(nftIds[i]),
+                address(fractron)
+            );
+            assertEq(address(vault.nftContracts[i]), address(nftContracts[i]));
+            assertEq(vault.tokenIds[i], nftIds[i]);
+        }
+
+        assertEq(vault.tokenSupply, 100 ether);
+        assertEq(vault.tokenContract.balanceOf(address(this)), 100 ether);
+    }
+
+    function testTotalSupplyOwnerCanJoinToken() public {
+        uint256 vaultId = fractron.split(
+            nftContracts,
+            nftIds,
+            100 ether,
+            "Fractionalised NFT",
+            "FRAC"
+        );
+
+        Fractron.Vault memory vault = fractron.getVault(vaultId);
+        for (uint256 i = 0; i < vault.nftContracts.length; i++) {
+            assertEq(
+                vault.nftContracts[i].ownerOf(vault.tokenIds[i]),
+                address(fractron)
+            );
+        }
+        assertEq(vault.tokenContract.balanceOf(address(this)), 100 ether);
+
+        vault.tokenContract.approve(address(fractron), type(uint256).max);
+        fractron.join(vaultId);
+
+        for (uint256 i = 0; i < vault.nftContracts.length; i++) {
+            assertEq(
+                vault.nftContracts[i].ownerOf(vault.tokenIds[i]),
+                address(this)
+            );
+        }
+        assertEq(vault.tokenContract.balanceOf(address(this)), 0);
+
+        vault = fractron.getVault(vaultId);
+        for (uint256 i = 0; i < vault.nftContracts.length; i++) {
+            assertEq(vault.tokenIds[0], 0);
+        }
+    }
+}
